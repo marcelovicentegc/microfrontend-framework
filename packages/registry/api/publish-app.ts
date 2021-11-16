baa77d79c9d38afa8960b49c37f12ec8486ed115
marcelovicentegc <marcelovicentegc@gmail.com>
2021-11-16 17:54:21 -0300

    chore(registry): prevent not allowed methods

--- packages/registry/api/publish-app.ts
+++ packages/registry/api/publish-app.ts
@@ -3,6 +3,13 @@ import { publish } from "libnpmpublish";
 import { Sentry } from "../middleware";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+
+    return res;
+  }
+
   const { manifest, tarData } = req.body;
 
   // TOOD: Only allowed client to post to this route is the @mf-framework/cli: add API Key authentication.
