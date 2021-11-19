6d4ec1f870e84eaf21006cd37347c31e1dca7ccb
marcelovicentegc <marcelovicentegc@gmail.com>
2021-11-19 15:58:44 -0300

    chore(templates): update @mf-framework/config from tenant template

--- packages/templates/tenant-template/mf-config.ts
+++ packages/templates/tenant-template/mf-config.ts
@@ -1,4 +1,8 @@
+import { AppConfig } from "@mf-framework/config";
+
 export default {
-  items: [{ title: "Dashboard", route: "/" }],
-  // [ITEMS ENTRY-POINT] THIS IS AN AUTOUPDATED FILE. DO NOT EDIT BELOW THIS LINE DIRECTLY
-};
+  items: [
+    { title: "Dashboard", route: "/" },
+    // [ITEMS ENTRY-POINT] THIS IS AN AUTOUPDATED FILE. DO NOT EDIT BELOW THIS LINE DIRECTLY
+  ],
+} as AppConfig;
