0484acf3aef798cdc1604e623f85a3951897983c
marcelovicentegc <marcelovicentegc@gmail.com>
2021-11-19 17:13:06 -0300

    feat(config): add pageName param

--- packages/config/src/types.ts
+++ packages/config/src/types.ts
@@ -1,8 +1,9 @@
 export interface AppConfig {
-  [key: string]: SidebarConfigTemplate[];
+  items: SidebarConfigTemplate[];
 }
 
 export interface SidebarConfigTemplate {
   route: string;
   title: string;
+  pageName: string;
 }
