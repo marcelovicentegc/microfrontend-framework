051d1701a489b3aff684934ae5e3e1ec5586aa08
Marcelo's personal machine <92038903+mypersonalmachine@users.noreply.github.com>
2021-11-20 17:13:37 -0300

    $chore(tenant:glowing-fiesta): install @mf-framework/mf-app-01@0.0.2

---
+++ examples/glowing-fiesta/pages/pricing.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Pricing from "../components/Pricing";
+
+const PricingPage: NextPage = () => {
+  return <Pricing />;
+};
+
+export default PricingPage;
