a0793e5fd9304799b6741d96cfbdcfe0cf6f0ac6
Marcelo's personal machine <92038903+mypersonalmachine@users.noreply.github.com>
2021-11-21 20:53:04 -0300

    chore(tenant:glowing-fiesta): install @mf-framework/mf-app-01@0.0.3

---
+++ examples/glowing-fiesta/pages/@mf-framework/mf-app-01@0.0.3/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Album from "../../../components/@mf-framework/mf-app-01@0.0.3/Album";
+
+const Home: NextPage = () => {
+  return <Album />;
+};
+
+export default Home;
