9866bfcbbde7bfbe01fde2aee3290b799aa5c403
marcelovicentegc <marcelovicentegc@gmail.com>
2021-11-21 11:00:51 -0300

    feat(examples): add glowing-fiesta tenant template

---
+++ examples/glowing-fiesta/mf-config.ts
@@ -0,0 +1,9 @@
+import { AppConfig } from "@mf-framework/config";
+
+export default {
+  items: [
+    { title: "Dashboard", route: "/" },
+    // THIS IS AN AUTOUPDATED FILE. DO NOT EDIT BELOW THIS LINE DIRECTLY
+    // [ITEMS ENTRY-POINT]
+  ],
+} as AppConfig;
