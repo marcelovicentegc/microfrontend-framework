5c43190905a276ad229529f3de8e076a2e3d3493
Marcelo's personal machine <92038903+mypersonalmachine@users.noreply.github.com>
2021-11-22 12:44:55 -0300

    chore(tenant:glowing-fiesta): install @mf-framework/mf-app-01@0.0.5

--- examples/glowing-fiesta/next.config.js
+++ examples/glowing-fiesta/next.config.js
@@ -5,6 +5,8 @@ module.exports = {
     return [
       // THIS IS AN AUTOUPDATED FILE. DO NOT EDIT BELOW THIS LINE DIRECTLY
       // [REWRITES ENTRY-POINT]
+{ source: "/mf-app-01/:path*",
+ destination: "/@mf-framework/mf-app-01@0.0.5/:path*", },
     ];
   },
 };
