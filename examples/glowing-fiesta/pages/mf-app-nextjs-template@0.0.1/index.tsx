49883db36ba1fcb9d9027e02685cf025816237fe
Marcelo's personal machine <92038903+mypersonalmachine@users.noreply.github.com>
2021-11-22 16:20:33 -0300

    chore(tenant:glowing-fiesta): install mf-app-nextjs-template@0.0.1

---
+++ examples/glowing-fiesta/pages/mf-app-nextjs-template@0.0.1/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import SignIn from "../../../components/mf-app-nextjs-template@0.0.1/SignIn";
+
+const Home: NextPage = () => {
+  return <SignIn />;
+};
+
+export default Home;
